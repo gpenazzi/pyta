305f1204f5417feed692954fab3f3ab33c325f3b
Gabriele Penazzi <gabriele.penazzi@bccms.uni-bremen.de>
2013-07-11 10:22:17 +0200

    Some pep8 fix

--- green/lead.py
+++ green/lead.py
@@ -2,14 +2,14 @@ import numpy as np
 import pyta.core.defaults as defaults
 import pyta.stats as stats
 
+
 class Lead:
     """A base class for managing and building up real and virtual leads
     (inscattering and outscattering sources)."""
 
-    def __init__(self, name, position, delta = defaults.delta):
+    def __init__(self, name, position, delta=defaults.delta):
         """A name to associate to the lead is always needed (e.g. 'left',
         'phonon')
-        
         position (int): index of interacting device layer"""
 
         self._type = None
@@ -36,12 +36,11 @@ class Lead:
         return self._position
 
 
-
 class MRDephasing(Lead):
     """A Lead modelling Momentum relaxing dephasing"""
 
-    def __init__(self, name, deph, eqgreen = None, green_gr = None):
-        """Only the name and dephasing intensity needed at first. 
+    def __init__(self, name, deph, eqgreen=None, green_gr=None):
+        """Only the name and dephasing intensity needed at first.
         You can provide the equilibrium or the Keldysh Green's function
     to get the self energies using set_eqgreen() and set_neqgreen()"""
         position = 0
@@ -82,22 +81,22 @@ class MRDephasing(Lead):
     def do_sigma(self):
         """Calculate the retarded self energy"""
         assert(not self._eqgreen is None)
-        tmp = np.matrix(np.eye(self._size), dtype = np.complex128)
-        np.fill_diagonal(tmp,self._eqgreen.diagonal())
+        tmp = np.matrix(np.eye(self._size), dtype=np.complex128)
+        np.fill_diagonal(tmp, self._eqgreen.diagonal())
         self._sigma = tmp * self._deph
 
     def do_sigma_gr(self):
         """Calculate the retarded self energy"""
         assert(not self._green_gr is None)
-        tmp = np.matrix(np.eye(self._size), dtype = np.complex128)
-        np.fill_diagonal(tmp,self._green_gr.diagonal())
+        tmp = np.matrix(np.eye(self._size), dtype=np.complex128)
+        np.fill_diagonal(tmp, self._green_gr.diagonal())
         self._sigma_gr = tmp * self._deph
 
     def do_sigma_lr(self):
         """Calculate the retarded self energy"""
         assert(not self._green_lr is None)
-        tmp = np.matrix(np.eye(self._size), dtype = np.complex128)
-        np.fill_diagonal(tmp,self._green_lr.diagonal())
+        tmp = np.matrix(np.eye(self._size), dtype=np.complex128)
+        np.fill_diagonal(tmp, self._green_lr.diagonal())
         self._sigma_lr = tmp * self._deph
 
     def get_sigma(self):
@@ -116,12 +115,11 @@ class MRDephasing(Lead):
         return self._sigma_lr
 
 
-
 class MCDephasing(Lead):
     """A Lead modelling Momentum conserving dephasing"""
 
-    def __init__(self, name, deph, eqgreen = None, green_gr = None):
-        """Only the name and dephasing intensity needed at first. 
+    def __init__(self, name, deph, eqgreen=None, green_gr=None):
+        """Only the name and dephasing intensity needed at first.
         You can provide the equilibrium or the Keldysh Green's function
     to get the self energies using set_eqgreen() and set_neqgreen()"""
         position = 0
@@ -172,7 +170,7 @@ class MCDephasing(Lead):
     def do_sigma_lr(self):
         """Calculate the greater self energy"""
         assert(not self._green_lr is None)
-        self._sigma_lr =  self._green_lr * self._deph 
+        self._sigma_lr = self._green_lr * self._deph
 
     def get_sigma(self):
         if self._sigma is None:
@@ -189,20 +187,23 @@ class MCDephasing(Lead):
             self.do_sigma_lr()
         return self._sigma_lr
 
+
 class PhysicalLead(Lead):
     """A class derived from Lead for the description of physical contacts"""
 
-    def __init__(self, name, ham, ham_t, ham_dl, position, over = None, over_t = None,
-    over_dl = None, mu = None, temp = 0.0, delta = defaults.delta):
-        """A PhysicalLead object describe a semi-infinite periodic lead within the Open Boundary 
+    def __init__(self, name, ham, ham_t, ham_dl, position, over=None,
+                 over_t=None, over_dl=None, mu=None, temp=0.0,
+                 delta=defaults.delta):
+        """A PhysicalLead object describe a semi-infinite periodic lead
+        within the Open Boundary
         Steady State picture. The following quantities must be specified:
-        
+
         ham (np.matrix): Hamiltonian for a single layer
         over (np.matrix), optional: Overlap for a single layer
         ham_t (np.matrix): coupling matrix between single layers
         over_t (np.matrix): overlap in the coupling block between single
         layers.If none, it's set tozero
-        ham_dl (np.matrix): coupling between lead and device 
+        ham_dl (np.matrix): coupling between lead and device
         over_lld (np.matrix): overlap in device-lead coupling
         position (int): index of interacting device layer
         mu (float): chemical potential
@@ -229,7 +230,7 @@ class PhysicalLead(Lead):
         #PL size n x n
         self._pl_size = self._ham.shape[0]
         #H must be a square matrix
-        assert( self._ham.shape[0] ==  self._ham.shape[1])
+        assert(self._ham.shape[0] == self._ham.shape[1])
         if not over:
             self._over = np.matrix(np.eye(self._pl_size))
         self._ham_t = ham_t
@@ -241,11 +242,11 @@ class PhysicalLead(Lead):
             self._over_dl = np.matrix(np.zeros(self._ham_dl.shape))
         self._delta = delta
 
-        #These variables are set to none if they have not been calculated, or set to
+        #These variables are set to none if they have not been calculated,
+        #or set to
         #none if something changed and their value is not valid anymore (es.
         #different energy point)
         self._sigma = None
-
 
     def set_energy(self, energy):
         """Set energy point"""
@@ -261,11 +262,12 @@ class PhysicalLead(Lead):
         """Set temperature, for nonequilibrium self energy"""
         self._temp = temp
 
-    def do_invsurfgreen(self, tol = defaults.surfgreen_tol):
-        """Calculate the INVERSE of surface green's function by means of decimation
+    def do_invsurfgreen(self, tol=defaults.surfgreen_tol):
+        """Calculate the INVERSE of surface green's function
+        by means of decimation
         algorithm Guinea F, Tejedor C, Flores F and Louis E 1983 Effective
         two-dimensional Hamiltonian at surfacesPhys. Rev.B 28 4397.
-        
+
         Note: from ASE implementation"""
 
         self._delta
@@ -286,12 +288,12 @@ class PhysicalLead(Lead):
             v_01 = -np.dot(v_01, a)
             v_10 = -np.dot(v_10, b)
             delta = abs(v_01).max()
-                
+
         return v_00
 
     def _do_sigma(self):
         """Calculate the equilibrium retarded self energy \Sigma^{r}."""
-        z = self._energy + self._delta * 1.j           
+        z = self._energy + self._delta * 1.j
         tau_dl = z * self._over_dl - self._ham_dl
         a_dl = np.linalg.solve(self.do_invsurfgreen(), tau_dl)
         tau_ld = z * self._over_dl.T.conj() - self._ham_dl.T.conj()
@@ -302,33 +304,31 @@ class PhysicalLead(Lead):
     def get_sigma_gr(self):
         """Calculate the Sigma greater"""
         assert(not self._mu is None)
-        return (1.0 - stats.fermi(self._energy, self._mu, temppot = self._temp)) * (-1j) * self.get_gamma()
+        return ((1.0 - stats.fermi(self._energy, self._mu, temppot=self._temp))
+                * (-1j) * self.get_gamma())
 
-    
     def get_sigma_lr(self):
         """Calculate the Sigma lesser"""
         assert(not self._mu is None)
-        sigma_lr =  stats.fermi(self._energy, self._mu, temppot = self._temp) * 1j * self.get_gamma()
+        sigma_lr = (stats.fermi(self._energy, self._mu, temppot=self._temp) *
+                    1j * self.get_gamma())
         return sigma_lr
-
 
     def get_inscattering(self):
         """Calculate the inscattering Sigma (Datta notation)"""
         assert(not self._mu is None)
         return stats.fermi(self._energy, self._mu) * self.get_gamma()
 
-    
     def get_outscattering(self):
         """Calculate the outscattering Sigma (Datta notation)"""
         assert(not self._mu is None)
         return (1.0 - stats.fermi(self._energy, self._mu)) * self.get_gamma()
 
-
     def get_sigma(self):
         if self._sigma is None:
             self._do_sigma()
         return self._sigma
-    
+
     def get_size(self):
         """Get size of Sigma"""
         return self._size
