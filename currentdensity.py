fixed some units conversion issue

currentdensity.py
@@ -3,6 +3,9 @@ import pyta.grid
 
 """Methods and class to build real space current density"""
 
+au_to_nm = 5.2918e-2
+au_to_ang = 5.2918e-1
+
 class CurrentDensity:
     """ A class to calculate and plot real space resolved current density"""
     def __init__(self, 
@@ -108,12 +111,11 @@ class CurrentDensity:
     
     def do_j_improved(self):
         """Calculate current density"""
-
         #FIND SOMETHING SMART HERE
         #For any nonzero density matrix couple define maximum and minimum
         #ii,jj,kk and then make a dictionary, cycle on the items and on
         #subblocks
-        local_cutoff = 8.0
+        #local_cutoff = 12.0
         for ii_nnz in range(self._weight.nnz):
             if np.mod(ii_nnz, 50) == 0:
                 print('nzval', ii_nnz, ' of ', self._weight.nnz)
@@ -122,9 +124,9 @@ class CurrentDensity:
             j_orb = self._weight.col[ii_nnz]
             if i_orb == j_orb:
                 continue
-            #i_storb = self._orb[self._orbind[i_orb]]
-            #j_storb = self._orb[self._orbind[j_orb]]
-            #local_cutoff = i_storb.get_cutoff() + j_storb.get_cutoff()
+            i_storb = self._orb[self._orbind[i_orb]]
+            j_storb = self._orb[self._orbind[j_orb]]
+            local_cutoff = i_storb.get_cutoff() + j_storb.get_cutoff()
             i_coord = self._orb_r[i_orb,:]
             j_coord = self._orb_r[j_orb,:]
             if ((abs(i_coord - j_coord) > local_cutoff).any()
@@ -154,7 +156,10 @@ class CurrentDensity:
                             self._grid.get_grid()[2][ind_min[2]:ind_max[2]+1]):
                         
                         coord = np.array([xx, yy, zz])
-                        tmp_j_vec = weight * self._do_currdensop(i_orb, j_orb, coord)
+                        #NOTE: units conversion in mA/nm^2
+                        #Assuming to get only the current operator from the density matrix
+                        conversion = 0.9839e2
+                        tmp_j_vec = weight * self._do_currdensop(i_orb, j_orb, coord) * conversion
                         self._j_vec[ii + ind_min[0], jj + ind_min[1], kk +
                                 ind_min[2], :] += tmp_j_vec
 
@@ -175,12 +180,17 @@ class CurrentDensity:
         return
 
     def surface_flux(self, axis=2, filename = 'flux.dat'):
+        """
+        Calculate the flux integral along x axis. We assume that
+        the current is evaluated in mA/nm^2 and return a value in mA
+        """
+        conversion = au_to_nm * au_to_nm * self._res * self._res
         j_axis = np.zeros(self._grid.get_npoints()[axis])
         if axis==2:
             for kk, zz in enumerate(self._grid.get_grid()[2]):
                 for ii, xx in enumerate(self._grid.get_grid()[0]):
                     for jj, yy in enumerate(self._grid.get_grid()[1]):
-                        j_axis[kk] = j_axis[kk] + self._j_vec[ii, jj, kk, 2]
+                        j_axis[kk] = j_axis[kk] + self._j_vec[ii, jj, kk, 2] * conversion
          
         print('j_axis', j_axis)
         with open(filename, 'w') as outfile:
@@ -195,7 +205,8 @@ class CurrentDensity:
             var = self._j_mag
 
         self._grid.dump_to_cube(var, filename=filename, 
-                header='Current Density Magnitude')
+                header='Current Density Magnitude',
+                conversion = au_to_ang)
         return
 
     def dump_to_vtk(self, var = None, filename = 'jmag.vtk'):
@@ -204,7 +215,8 @@ class CurrentDensity:
             var = self._j_mag
 
         self._grid.dump_to_vtk(var, filename=filename,
-                                header='Current Density Magnitude')
+                                header='Current Density Magnitude',
+                                conversion = au_to_ang)
         return
 
     def dump_vec_to_vtk(self, var = None, filename = 'jmag.vtk'):
@@ -213,7 +225,8 @@ class CurrentDensity:
             var = self._j
 
         self._grid.dump_vec_to_vtk(var, filename=filename,
-                               header='Current Density Magnitude')
+                               header='Current Density Magnitude',
+                               conversion = au_to_ang)
         return
 
 

grid.py
@@ -81,7 +81,6 @@ class CubicGrid:
         else:
             rmin = rmin - tol
             rmax = rmax + tol
-            
 
         return (rmin, rmax)
 
@@ -101,20 +100,21 @@ class CubicGrid:
             self._nnodes *= npoints
         return
 
-    def dump_to_cube(self, var, filename = 'tmp.cube', header = "Created with PYTA"):
+    def dump_to_cube(self, var, filename = 'tmp.cube', header = "Created with PYTA",
+                     conversion = 1.0):
         """Write orbital to cube file """
         with open(filename, 'w') as outfile:
             #REMINDER: in cube file negative voxel length means Angstrom Units
                 outfile.write('Pyta cube file \n')
                 outfile.write(header + '\n')
-                outfile.write('{} {} {} {} \n'.format(1, self._rmin[0],
-                    self._rmin[1], self._rmin[2]))
+                outfile.write('{} {} {} {} \n'.format(1, self._rmin[0] * conversion,
+                    self._rmin[1] * conversion, self._rmin[2] * conversion))
                 outfile.write('{} {} {} {}\n'.format(self._npoints[0], 
-                    self._step[0], 0.0, 0.0))
+                    self._step[0] * conversion, 0.0, 0.0))
                 outfile.write('{} {} {} {}\n'.format(self._npoints[1], 
-                    0.0, self._step[1], 0.0))
+                    0.0, self._step[1] * conversion, 0.0))
                 outfile.write('{} {} {} {}\n'.format(self._npoints[2], 
-                    0.0, 0.0, self._step[2]))
+                    0.0, 0.0, self._step[2] *  conversion))
                 outfile.write('{} {} {} {} {} \n'.format(1, 0.0, 0.0, 0.0, 0.0))
                 for ii in range(self._npoints[0]):
                     for jj in range(self._npoints[1]):
@@ -122,7 +122,7 @@ class CubicGrid:
                             outfile.write('{}  '.format(
                                 var[ii, jj, kk]))
 
-    def dump_to_vtk(self, var, filename = 'tmp.cube', header = "var"):
+    def dump_to_vtk(self, var, filename = 'tmp.cube', header = "var", conversion = 1.0):
         """Write orbital to cube file """
         with open(filename, 'w') as outfile:
             #REMINDER: in cube file negative voxel length means Angstrom Units
@@ -134,11 +134,13 @@ class CubicGrid:
             outfile.write('{} {} {}\n'.format(self._npoints[0],
                                               self._npoints[1], self._npoints[2]))
             outfile.write('SPACING ')
-            outfile.write('{} {} {}\n'.format(self._step[0],
-                                              self._step[1], self._step[2]))
+            outfile.write('{} {} {}\n'.format(self._step[0] * conversion,
+                                              self._step[1] * conversion,
+                                              self._step[2] * conversion))
             outfile.write('ORIGIN ')
-            outfile.write('{} {} {}\n'.format(self._rmin[0],
-                                              self._rmin[1], self._rmin[2]))
+            outfile.write('{} {} {}\n'.format(self._rmin[0] * conversion,
+                                              self._rmin[1] * conversion,
+                                              self._rmin[2] * conversion))
             outfile.write('POINT_DATA ')
             outfile.write('{}\n'.format(self._npoints[0] * self._npoints[1]
                                         * self._npoints[2]))
@@ -150,7 +152,7 @@ class CubicGrid:
                         outfile.write('{}\n'.format(
                             var[kk, jj, ii]))
 
-    def dump_vec_to_vtk(self, var, filename = 'tmp.cube', header = "var"):
+    def dump_vec_to_vtk(self, var, filename = 'tmp.cube', header = "var", conversion = 1.0):
         """Write orbital to cube file """
         with open(filename, 'w') as outfile:
             #REMINDER: in cube file negative voxel length means Angstrom Units
@@ -162,7 +164,7 @@ class CubicGrid:
             for ii in range(self._npoints[2]):
                 for jj in range(self._npoints[1]):
                     for kk in range(self._npoints[0]):
-                        coord = self.get_space_coord(np.array([kk,jj,ii]))
+                        coord = self.get_space_coord(np.array([kk,jj,ii])) * conversion
                         outfile.write('{} {} {}\n'.format(
                             coord[0], coord[1], coord[2]))
             outfile.write('\nCELLS {} {}\n'.format(self._nnodes, 2*self._nnodes))